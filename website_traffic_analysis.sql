668e67aa12f1fd908f5914444ae78d11cb869efa
Yi WEI <yiwei3882@gmail.com>
2024-12-17 12:42:30 +0100

    Update

--- website_traffic_analysis.sql
+++ website_traffic_analysis.sql
@@ -1,1 +1,9 @@
+# Create the scripts folder
+mkdir scripts
+
+# Move or create SQL files under scripts
+touch scripts/traffic_analysis.sql
+touch scripts/bounce_rate_analysis.sql
+touch scripts/conversion_funnels.sql
+touch scripts/ab_testing.sql
 
